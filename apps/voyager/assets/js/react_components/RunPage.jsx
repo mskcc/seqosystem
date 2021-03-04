c97b999dbf9966d50e5768f31e53aafe97816af8
Nikhil <nikhilkumar516@gmail.com>
2021-03-04 10:50:59 -0500

    Reduced run limit even more

--- apps/voyager/assets/js/react_components/RunPage.jsx
+++ apps/voyager/assets/js/react_components/RunPage.jsx
@@ -230,7 +230,7 @@ const RunJobs = function (props) {
 
 export default function RunPage(props) {
     const classes = useStyles();
-    const runListDaysAgo = 7;
+    const runListDaysAgo = 2;
     const { runList, runJobsRoute, handleEvent, pushEvent } = props;
     const [runs, updateRuns] = useState(runList);
 
