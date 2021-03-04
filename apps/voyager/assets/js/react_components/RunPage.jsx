928853967fb60e60fce981ef8cd5b039f470b525
Nikhil <nikhilkumar516@gmail.com>
2021-03-04 11:22:43 -0500

    Reduced runs even more

--- apps/voyager/assets/js/react_components/RunPage.jsx
+++ apps/voyager/assets/js/react_components/RunPage.jsx
@@ -230,7 +230,7 @@ const RunJobs = function (props) {
 
 export default function RunPage(props) {
     const classes = useStyles();
-    const runListDaysAgo = 2;
+    const runListDaysAgo = 1;
     const { runList, runJobsRoute, handleEvent, pushEvent } = props;
     const [runs, updateRuns] = useState(runList);
 
